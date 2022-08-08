840971ba3ac1416717a576066e96df8fac21c36e
Jasmin Dedic <jdedic1999@gmail.com>
2022-08-08 12:05:16 +0200

    Finished expanding cards section

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ toggleButton.addEventListener("click", () => {
     navbarLinks1.classList.toggle("active");
     navbarLinks2.classList.toggle("active");
 })
+
+
+// Event listener for the expanding cards. When the user clicks on one of the cards immediately the active  
+// class gets assigned to that card. 
+let panels = document.querySelectorAll(".panel");
+
+for (let i = 0; i < panels.length; i++) {
+    panels[i].addEventListener("click", () => {
+        removeActiveClasses();
+        panels[i].classList.add("active");
+    })
+}
+
+// Function that removes all active classes from panels
+function removeActiveClasses(){
+    for (let i = 0; i < panels.length; i++){
+            panels[i].classList.remove("active");
+    }
+}
